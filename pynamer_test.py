fb325efc56fca59590e8fdefff24833136f645a3
Stavros Filosidis <stavrosfil@gmail.com>
2019-09-19 23:47:14 +0300

    Improve tests

--- pynamer_test.py
+++ pynamer_test.py
@@ -2,6 +2,14 @@ import pytest
 import pynamer
 
 
-@pytest.mark.parametrize("text, text_id", [('testerino12.test', 12), ('123test321.exe', 123), ('123test.py', 123), ('', -1)])
+@pytest.mark.parametrize("text, text_id", [('testerino12.test', 12),
+                                           ('123t est321.exe', 123),
+                                           ('123test.py', 123), ('', -1)])
 def test_extract_id(text, text_id):
     assert pynamer.extract_id(text) == text_id
+
+
+# @pytest.mark.parametrize("prefix, extensions", [
+#     ('testPrefix', )
+# ])
+# def test_files_to_rename(prefix, extensions):
